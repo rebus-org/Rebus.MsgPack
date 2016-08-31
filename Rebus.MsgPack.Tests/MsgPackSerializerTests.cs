67d2c7f139c1e2c88a88e580593619fd9dd1e34c
Mogens Heller Grabe <mookid8000@gmail.com>
2016-08-31 11:01:16 +0200

    now you are here

---
+++ Rebus.MsgPack.Tests/MsgPackSerializerTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+using Rebus.Tests.Contracts.Serialization;
+
+namespace Rebus.MsgPack.Tests
+{
+    [TestFixture]
+    public class MsgPackSerializerTests : BasicSerializationTests<MsgPackSerializerFactory>
+    {
+    }
+}
